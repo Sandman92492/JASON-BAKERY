1581818b6ddb19f115bd1e724a2dfa2c9931e7e1
Sandman92492 <imagenbeats@gmail.com>
2025-10-11 12:44:40 +0200

    Add PWA service worker registration and icon

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,3 +3,11 @@ import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(reg => console.log('Service Worker registered'))
+      .catch(err => console.log('Service Worker registration failed:', err));
+  });
+}
